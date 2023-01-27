7aa5c28642ec0f204a7849069ebff741d4b91b6c
DebugHyena <maciej.staniec@yahoo.co.uk>
2023-01-27 09:19:25 +0000

    commit

--- 12. APIs/script.js
+++ 12. APIs/script.js
@@ -3,4 +3,5 @@ const countryCode = "GB";
 const apiKey = "5535ef8ca0e0e33a597c83028393f925";
 
 geoCoordinates = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${countryCode}&limit=5&appid=${apiKey}`;
-// http://api.openweathermap.org/geo/1.0/direct?q=London,GB&limit=5&appid=d2a36e51d2d734ca7a9fd9403af33309
+
+console.log(geoCoordinates);
