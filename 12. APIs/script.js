6c8fb9ccfb69d4b2b630c530952fa38003bc3a1e
DebugHyena <maciej.staniec@yahoo.co.uk>
2023-02-07 09:00:33 +0000

    commit

--- 12. APIs/script.js
+++ 12. APIs/script.js
@@ -1,9 +1,20 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const https = require('https')
+const express = require("express");
+const https = require("https");
+const { allowedNodeEnvironmentFlags } = require("process");
 const app = express();
 
-units = ['standard', 'imperial', 'metric']
+const units = ["standard", "imperial", "metric"];
+const apiKey = "5535ef8ca0e0e33a597c83028393f925";
+let city = "";
 
-const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`
+const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
