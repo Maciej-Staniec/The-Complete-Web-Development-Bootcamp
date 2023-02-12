700da6a3b4805863aa7c3fce026b5913a777c06d
DebugHyena <maciej.staniec@yahoo.co.uk>
2023-02-12 09:57:42 +0000

    commit

--- 12. APIs/mailchimp/app.js
+++ 12. APIs/mailchimp/app.js
@@ -1,12 +1,23 @@
 "use strict";
 
+const bodyParser = require("body-parser");
 const express = require("express");
 const https = require("https");
+const path = require("path");
 
 const app = express();
 
+// If we don't use the following code, it won't load css file.
+app.use(express.static(path.join(__dirname)));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.sendFile(path.join(__dirname, "signup.html"));
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(3000, () => {
