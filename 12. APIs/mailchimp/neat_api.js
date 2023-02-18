bdc30413f85b29961a00354c266f8cab21c59964
DebugHyena <maciej.staniec@yahoo.co.uk>
2023-02-18 10:59:00 +0000

    commit

--- 12. APIs/mailchimp/neat_api.js
+++ 12. APIs/mailchimp/neat_api.js
@@ -1,14 +1,22 @@
 "use strict";
 
 const express = require("express");
-const https = require("https");
-const bodyParser = require("body-parser");
-const path = require("path");
-
 const app = express();
+const https = require("https");
+// From express 4.16.0 version, there is a built-in middleware function, that parses incoming request bodies with urlencoded payloads (data in the form "key=value&key=value"). It also adds the 'body' object to the request object, containing parsed data. Thanks to that, we can access the user's input using the 'req.body.x' syntax.
+// Let's comment out 'require("body-parser")', as we don't need to import this module anymore.
+// const bodyParser = require("body-parser");
+
+// Instead, let's use a built-in middleware function to parse urlencoded payloads.
+// Any data submitted by a user on a website comes as a url-encoded payload. Therefore, we have to use the following statement:
+app.use(express.urlencoded({ extended: true }));
+
+// We've got to use a middleware function, which enables users to get all files required to render our web page. The express.static() function does this job.
+const path = require("path");
+app.use(express.static(path.join(__dirname)));
 
 const listID = `ec47cb7864`;
-const apiKey = "your_API_code";
+const apiKey = "your_API_key";
 
 const listOptions = {
   // We don't need to specify the request method, as mailchimp doesn't require to do so, but some APIs might still need this to be specified.
@@ -26,8 +34,8 @@ const listOptions = {
   },
 };
 
-// To make sure that we get data from the API server before proceding to execute the rest of the code, we've got to use Promises, as any https request is asynchronous. Promise
-function httpsRequest(options) {
+// To make sure that we get data from the API server before proceding to execute the rest of the code, we've got to use Promises, as any https request is asynchronous.
+function httpsGetRequest(options) {
   return new Promise((resolve, reject) => {
     let data = [];
     const request = https.request(options, (response) => {
@@ -53,7 +61,42 @@ function httpsRequest(options) {
       console.log(`request.on(error)`);
       reject(error);
     });
+
     // Because we use https.request() method, not https.get(), we have to end the request explicitly using the .end() method. https.request() is more flexible, as we can specify request options usually packed in an object, just like we did using the 'listInfoOptions'. On the other hand, the https.get() is a GET request. In this case, we don't have to specify the type of request.
+    request.end();
+  });
+}
+
+function httpsPostRequest(options, userData) {
+  return new Promise((resolve, reject) => {
+    let data = [];
+
+    const request = https.request(options, (response) => {
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      response.on("end", () => {
+        const response = JSON.parse(data);
+        console.log(response.status);
+        if (response.status < 200 || response.status > 300) {
+          reject(response);
+        } else {
+          resolve(response);
+        }
+      });
+
+      response.on("error", (error) => {
+        reject(error);
+        console.log("There was a problem with the response from the server.");
+      });
+    });
+    request.on("error", (error) => {
+      console.log(`There was a problem with the HTTP request, details:
+      ${error}`);
+    });
+
+    request.write(userData);
     request.end();
   });
 }
@@ -85,6 +128,44 @@ const memberRequestOptions = {
   },
 };
 
-printHttpsRequest(listOptions);
-printHttpsRequest(memberRequestOptions);
+const addNewMemberOptions = {
+  host: "us17.api.mailchimp.com",
+  path: `/3.0/lists/${listID}/members`,
+  method: "POST",
+  auth: `apikey:${apiKey}`,
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
 
+// printHttpsRequest(listOptions);
+// printHttpsRequest(memberRequestOptions);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const data = {
+    email_address: req.body.email,
+    status: "subscribed",
+    merge_fields: {
+      FNAME: req.body.fname,
+      LNAME: req.body.lname,
+    },
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  httpsPostRequest(addNewMemberOptions, jsonData)
+    .then((data) => {
+      res.sendFile(__dirname + "/success.html");
+    })
+    .catch((error) => {
+      res.sendFile(__dirname + "/failure.html");
+    });
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000.");
+});
